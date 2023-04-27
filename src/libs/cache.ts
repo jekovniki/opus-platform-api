bbdb5da4e0b85b9ce5f43b4de5267a64b6413abd
nikolayz <nikolay.zhekov@onseostudio.com>
2023-04-27 16:06:56 +0300

    XXXX | Redis  + docker [+] Add Dockerfile [+] add docker-compose [+] add Cache class

---
+++ src/libs/cache.ts
@@ -0,0 +1,51 @@
+import { Redis, RedisKey} from "ioredis";
+import { CACHE } from "../utils/configuration";
+import { logger } from "./logger";
+
+class Cache {
+    protected client: Redis;
+    protected port: number;
+    protected host: string;
+    protected readyStatus = "ready";
+    protected connectStatus = "connect";
+
+    constructor(port: number, host: string) {
+        this.port = port;
+        this.host = host;
+        this.client = new Redis(this.port, this.host);
+    }
+
+    public getClient(): Redis {
+        return this.client;
+    }
+
+    public async get(key: RedisKey): Promise<string | null> {
+        return this.client.get(key, (error, result) => {
+            if (error) {
+                return null;
+            }
+
+            return result;
+        })
+    }
+
+    public async set(key: RedisKey, value: string | number | Buffer): Promise<string | undefined> {
+        return this.client.set(key, value);
+    }
+
+    public async remove(keys: RedisKey[]): Promise<number> {
+        return this.client.del(keys);
+    }
+
+    public status(): string {
+        if (this.client.status === this.readyStatus || this.client.status === this.connectStatus) {
+            logger.info(`Connected successfully to memory database(Redis) on: ${this.host}:${this.port}`);
+        } else {
+            logger.error(`Could not connected to memory database(Redis) on: ${this.host}:${this.port} | Current status is ${this.client.status}`);
+        }
+
+        return this.client.status;
+    }
+}
+
+export const cache = new Cache(CACHE.PORT, CACHE.HOST);
--- src/utils/constants/errors.ts
+++ src/utils/constants/errors.ts
@@ -1,1 +1,5 @@
 export const ENVIRONMENT_VARIABLE = "Environment variable is undefined! Check for typo errors or add the missing variable";
+
+export const UNHANDLED_REJECTION_ERROR = "Unhandled Rejection Error at promise:";
+
+export const UNCAUGHT_EXCEPTION_ERROR = "Uncaught Exception Error at origin:";
